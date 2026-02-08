6714e1fca840699d36793049c92f280c90942dfb
kaleem-ins <kaleemkhan@insightcorp.health>
2026-02-08 21:49:49 +0500

    fix: simplify metro config to avoid Node compatibility issues

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,5 @@
 /* eslint-env node */
 const { getDefaultConfig } = require('@expo/metro-config');
-const { FileStore } = require('metro-cache');
-const os = require('os');
-const path = require('path');
 
 /** @type {import('@expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
@@ -19,15 +16,6 @@ config.resolver = {
   ...config.resolver,
   assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
   sourceExts: [...config.resolver.sourceExts, 'svg'],
-
-  unstable_conditionNames: ['require', 'react-native', 'default'],
 };
-
-// Cache configuration
-config.cacheStores = [
-  new FileStore({
-    root: path.join(os.tmpdir(), 'metro-cache'),
-  }),
-];
 
 module.exports = config;
